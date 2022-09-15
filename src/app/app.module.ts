df330d780866f1480d0d7e1964dcf5e71b8c4f8d
Carl Meyer <carl.meyer@paxton.co.uk>
2022-09-15 16:06:10 +0100

    Toolbar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RocketComponent } from './rocket/rocket.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RocketComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
