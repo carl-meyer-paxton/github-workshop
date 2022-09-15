2720c3f6c9294095af7a893b68d6d3349639df43
Carl Meyer <carl.meyer@paxton.co.uk>
2022-09-15 16:13:27 +0100

    Resources Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RocketComponent } from './rocket/rocket.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RocketComponent,
     ToolbarComponent,
+    ResourcesComponent,
   ],
   imports: [
     BrowserModule,
